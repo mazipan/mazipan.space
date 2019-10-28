ac71c983c84689eed6c7a61f96b9c02910992b66
Paul Miller <paul@paulmillr.com>
2019-10-28 17:35:06 +0700

    feat: add gists

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,13 @@ module.exports = {
               inlineCodeMarker: '÷',
             },
           },
+          {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              username: 'mazipan',
+              includeDefaultCss: true
+            }
+          }
         ],
       },
     },
