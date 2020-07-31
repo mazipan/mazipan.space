4f30e1602b36422495d5b92146ea1842f9b55bc0
Irfan Maulana <mazipanneh@gmail.com>
2020-07-31 20:10:19 +0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ const tagTemplate = path.resolve('./src/templates/tags.tsx');
 const authorTemplate = path.resolve('./src/templates/author.tsx');
 const postTemplate = path.resolve('./src/templates/post.tsx');
 
-const POST_PER_PAGE = 6;
+const POST_PER_PAGE = 7;
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField  } = actions;
