362c1b43b041fb5d3ec564adad57d2b6cb619140
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-10-18 12:58:07 +0700

    feat: next

---
+++ next/blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'Next.js Starter Blog',
+    author: 'Irfan Maulana',
+    image: '/static/site-feature.png',
+    description: 'Next.js starter blog',
+    siteUrl: 'https://nextjs-blog-starter.now.sh',
+    social: {
+      twitter: 'maz_ipan',
+    },
+    postsPerPage: 10,
+  },
+};
