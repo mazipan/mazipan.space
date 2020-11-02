eb8e6a0bb697afa02083e00ca7aa2c00c6c68bfc
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-11-02 09:09:25 +0700

    feat: add line highlight

--- components/Markdown/CodeBlock.js
+++ components/Markdown/CodeBlock.js
@@ -1,19 +1,39 @@
 import PropTypes from 'prop-types'
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import rangeParser from 'parse-numeric-range'
 import theme from 'prism-react-renderer/themes/nightOwl'
 
-function CodeBlock ({ language = null, value }) {
+const calculateLinesToHighlight = (meta) => {
+  const RE = /{([\d,-]+)}/
+  if (RE.test(meta)) {
+    const strlineNumbers = RE.exec(meta)[1]
+    const lineNumbers = rangeParser(strlineNumbers)
+    return (index) => (lineNumbers.includes(index + 1))
+  } else {
+    return () => false
+  }
+}
+
+function CodeBlock ({ language = null, codeString, metastring }) {
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+
   return (
-    <Highlight {...defaultProps} theme={theme} code={value} language={language}>
+    <Highlight {...defaultProps} theme={theme} code={codeString} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })} key={i}>
+          {tokens.map((line, i) => {
+            const lineProps = getLineProps({ line, key: i })
+            if (shouldHighlightLine(i)) {
+              lineProps.className = `${lineProps.className} highlight-line`
+            }
+
+            return (<div {...lineProps} key={i}>
               {line.map((token, key) => (
                 <span {...getTokenProps({ token, key })} key={key} />
               ))}
-            </div>
-          ))}
+            </div>)
+          }
+          )}
         </pre>
       )}
     </Highlight>
