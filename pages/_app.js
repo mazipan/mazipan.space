316a4bbea9305878b63d753b8929f0c684a8060e
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-11-05 09:40:55 +0700

    chore: add web vital report

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/index.css'
 
+// https://nextjs.org/docs/advanced-features/measuring-performance#sending-results-to-analytics
+export function reportWebVitals ({ id, name, label, value }) {
+  window.gtag('event', name, {
+    event_category:
+      label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true
+  })
+}
+
 export default function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
