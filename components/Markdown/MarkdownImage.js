f5628c78d0921a7ff75875d28c73995af8955c84
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-01-18 17:28:10 +0700

    fix: image on post detail

--- components/Markdown/MarkdownImage.js
+++ components/Markdown/MarkdownImage.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-
 function MarkdownImage ({ src, alt }) {
+  const nextImage2x = `/_next/image?url=${encodeURIComponent(src)}&w=1200&q=75`
+  const nextImage1x = `/_next/image?url=${encodeURIComponent(src)}&w=768&q=75`
+
   return (
     <div className="flex justify-center items-center">
       <figure>
-        <Image src={src} alt={alt} layout="fill" className="object-cover"/>
+        <img loading="lazy" src={nextImage2x} alt={alt} srcSet={`${nextImage1x} 1x, ${nextImage2x} 2x`}/>
         <figcaption>{alt}</figcaption>
       </figure>
     </div>
