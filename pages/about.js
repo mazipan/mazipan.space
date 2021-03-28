cc0013a74050b90686c6a003923e3af25e49d409
Irfan Maulana <mazipanneh@gmail.com>
2021-03-28 14:26:24 +0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -20,16 +20,18 @@ export default function Index () {
             About
           </h2>
           <div className="content">
-            <h2 className="text-3xl md:text-4xl font-bold">Hi 👋, I am Irfan Maulana.</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-2">Hi 👋, I am Irfan Maulana.</h2>
             <p>
               A frontend engineer, now work in Tokopedia as Principal Engineer Web Platform.
-              I start my software engineering career since 2013.
-              Before joining Tokopedia, I have work on several companies such as Bizzy Indonesia, Blibli.com and SML Technologies.  
+              I already start my software engineering careers since 2013.
+              Before joining Tokopedia, I work on several companies such as Bizzy Indonesia, Blibli.com and SML Technologies.  
             </p>
+            <br/>
             <p>
               I used to build the web using various JavaScript framework, or I can say that I am not fanatic with any frameworks out there.
-              I have experience in writing many custom configuration for the build tools for the front end codebase. You just name it, webpack, rollup, or any other build tools, I can work with it (or at least I can learn it easily if I don't know it yet).
+              I have experience in writing many custom configuration for the build tools for the front end codebase. You just name it, webpack, rollup, or any other build tools, I can work with it (<i>or at least I can learn it easily if I don't know it yet<i/>).
             </p>
+            <br/>
             <p>
               Outside my job as a full time employee, I also build a close connection with many programmer communities in Indonesia to give back my knowledge and experience in the web development to the other engineers.
             </p>
