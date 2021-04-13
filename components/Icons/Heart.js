0f625ec344895fc081b33fd3398b7a1e5c44eae7
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-04-14 00:12:47 +0700

    chore: rename

--- components/Icons/Heart.js
+++ components/Icons/Heart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Icon = () => {
+const HeartIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -18,4 +18,4 @@ const Icon = () => {
   )
 }
 
-export default Icon
+export default HeartIcon
