bf27114b3c47ab0cb48c9f97478b2e0f034ec2cb
Irfan Maulana <mazipanneh@gmail.com>
2021-04-17 17:22:31 +0700

    🔥 z-index

--- components/FloatingNav/Default.js
+++ components/FloatingNav/Default.js
@@ -8,7 +8,7 @@ import ActiveLink from './ActiveLink'
 
 export default function FloatingNav () {
   return (
-    <nav className="md:hidden fixed w-full bottom-0 left-0 p-4 z-10 bg-gray-100 dark:bg-gray-900 flex items-center justify-between shadow-medium border-top-gradient">
+    <nav className="md:hidden fixed w-full bottom-0 left-0 p-4 z-20 bg-gray-100 dark:bg-gray-900 flex items-center justify-between shadow-medium border-top-gradient">
       <div className="w-full flex items-center justify-between">
         <ActiveLink href="/">
           <>
