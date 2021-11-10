229dad3d370e280a04908222dbc85eb177095407
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2021-11-10 20:36:18 +0700

    fix: eslint for next

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  env: {
-    browser: true,
-    es2021: true
-  },
   extends: [
-    'plugin:@next/next/recommended'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
+    'next'
+  ]
 }
