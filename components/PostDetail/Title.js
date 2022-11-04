ad9098b43c3df824439c49feea652760c5c64fcb
Irfan Maulana <mazipanneh@gmail.com>
2022-11-04 08:28:49 +0700

    FIX: remove leading-none

--- components/PostDetail/Title.js
+++ components/PostDetail/Title.js
@@ -1,6 +1,6 @@
 export default function PostTitle ({ children }) {
   return (
-    <h1 className="relative text-3xl md:text-4xl lg:text-5xl text-gradient font-heading font-bold tracking-tighter leading-tight md:leading-none mt-8 text-center md:text-left">
+    <h1 className="relative text-3xl md:text-4xl lg:text-5xl text-gradient font-heading font-bold tracking-tighter leading-tight mt-8 text-center md:text-left">
       {children}
     </h1>
   )
