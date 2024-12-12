2a5d4e7b926134df6acfb8d8e841f52759759e7e
Irfan Maulana <mazipanneh@gmail.com>
2024-12-12 20:54:34 +0700

    Happy new year 2025

---
+++ src/constants/profile.ts
@@ -0,0 +1,10 @@
+export const PROFILE = {
+  TAGLINE:
+    'Web developer with extensive experience in building projects from the ground up, enhancing performance and scalability, managing tech stack migrations, promoting quality-focused practices, and mentoring junior developers.',
+  CURRENT_POSITION: 'Part of GovTech Edu Indonesia.',
+  HIGHTLIGHT: [
+    '⚛️ React.js x TypeScript',
+    '💼 Ex: Sayurbox, Tokopedia, Bizzy Indonesia, Blibli, & SML Technologies.',
+    '🛟 Active contributor to open source project in PHPID, React.js ID, & KawalCovid19.',
+  ],
+};
